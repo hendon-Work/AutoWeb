6d2a00dc9ad920a420914e52e9bd85f0aa576297
jayden.coys <winmymel@gmail.com>
2026-03-17 14:27:24 +0900

    test: update login e2e tests

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -9,12 +9,12 @@ test.describe('로그인 페이지 유효성 검사 테스트', () => {
         await loginPage.goto();
     });
 
-    test('정상적인 계정으로 로그인 성공 테스트', { 
+    test('정상적인 계정으로 로그인 성공 테스트', {
         tag: '@P0',
         annotation: [
             { type: '2depth', description: '로그인' },
             { type: '3depth', description: '로그인 성공' },
-            { type: 'precondition', description: '로그인 페이지 접속 완료' },
+            { type: 'precondition', description: '로그인 페이지(https://www.saucedemo.com/) 접속 완료' },
             { type: 'teststep', description: '유효한 아이디/비밀번호 입력 후 로그인 클릭' },
             { type: 'expectedresult', description: '상품 목록(inventory) 페이지로 정상 이동됨' }
         ]
@@ -23,12 +23,12 @@ test.describe('로그인 페이지 유효성 검사 테스트', () => {
         await expect(page).toHaveURL(/.*inventory/);
     });
 
-    test('아이디를 입력하지 않고 로그인 시도', { 
+    test('아이디를 입력하지 않고 로그인 시도', {
         tag: '@P1',
         annotation: [
             { type: '2depth', description: '로그인' },
             { type: '3depth', description: '입력 유효성' },
-            { type: 'precondition', description: '로그인 페이지 접속 완료' },
+            { type: 'precondition', description: '로그인 페이지(https://www.saucedemo.com/) 접속 완료' },
             { type: 'teststep', description: '아이디 빈칸 상태에서 비밀번호 입력 후 로그인 클릭' },
             { type: 'expectedresult', description: 'Username is required 에러 메시지가 표시됨' }
         ]
@@ -37,12 +37,12 @@ test.describe('로그인 페이지 유효성 검사 테스트', () => {
         await expect(loginPage.getErrorMessage()).toHaveText('Epic sadface: Username is required');
     });
 
-    test('비밀번호를 입력하지 않고 로그인 시도', { 
+    test('비밀번호를 입력하지 않고 로그인 시도', {
         tag: '@P1',
         annotation: [
             { type: '2depth', description: '로그인' },
             { type: '3depth', description: '입력 유효성' },
-            { type: 'precondition', description: '로그인 페이지 접속 완료' },
+            { type: 'precondition', description: '로그인 페이지(https://www.saucedemo.com/) 접속 완료' },
             { type: 'teststep', description: '아이디 입력 후 비밀번호 빈칸 상태에서 로그인 클릭' },
             { type: 'expectedresult', description: 'Password is required 에러 메시지가 표시됨' }
         ]
@@ -51,7 +51,7 @@ test.describe('로그인 페이지 유효성 검사 테스트', () => {
         await expect(loginPage.getErrorMessage()).toHaveText('Epic sadface: Password is required');
     });
 
-    test('일치하지 않는 계정 정보로 로그인 시도', { 
+    test('일치하지 않는 계정 정보로 로그인 시도', {
         tag: '@P1',
         annotation: [
             { type: '2depth', description: '로그인' },
@@ -65,12 +65,12 @@ test.describe('로그인 페이지 유효성 검사 테스트', () => {
         await expect(loginPage.getErrorMessage()).toHaveText('Epic sadface: Username and password do not match any user in this service');
     });
 
-    test('블락(잠긴) 처리된 계정으로 로그인 시도', { 
+    test('블락(잠긴) 처리된 계정으로 로그인 시도', {
         tag: '@P2',
         annotation: [
             { type: '2depth', description: '로그인' },
             { type: '3depth', description: '계정 상태' },
-            { type: 'precondition', description: '로그인 페이지 접속 완료' },
+            { type: 'precondition', description: '로그인 페이지(https://www.saucedemo.com/) 접속 완료' },
             { type: 'teststep', description: '잠긴 계정(locked_out_user) 정보 입력 후 로그인 클릭' },
             { type: 'expectedresult', description: 'user has been locked out 에러 메시지가 표시됨' }
         ]
